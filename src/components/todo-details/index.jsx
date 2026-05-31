b353ecc41be906c7945fc65dcc4a237a1325d5c1
karthik032507 <karthikeya0325@gmail.com>
2026-05-31 17:04:02 +0530

    Add styling to close button in TodoDetails

--- src/components/todo-details/index.jsx
+++ src/components/todo-details/index.jsx
@@ -1,5 +1,6 @@
 import { Button, Dialog, DialogTitle, DialogActions } from "@mui/material";
 import { Fragment } from "react";
+import styles from "../../style.module.css";
 
 export default function TodoDetails({
     todoDetails, openDialog, setOpenDialog, setTodoDetails
@@ -8,7 +9,7 @@ export default function TodoDetails({
         <Dialog onClose={()=>setOpenDialog(false)}   open={openDialog}>
             <DialogTitle>{todoDetails?.todo}</DialogTitle>
             <DialogActions>
-                <button  onClick={()=>{
+                <button className={styles.closeButton} onClick={()=>{
                     setTodoDetails(null);
                     setOpenDialog(false);
                 }}>Close</button>
